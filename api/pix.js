9df07889cdc057387fdafd42eae242bf7a8c193f
carlosmarleynx7 <carlosmarleycastro852@gmail.com>
2026-05-27 22:10:00 -0300

    Update pix.js

--- api/pix.js
+++ api/pix.js
@@ -1,49 +1,31 @@
 import mercadopago from "mercadopago";
 
 mercadopago.configure({
-  access_token: process.env.MERCADO_PAGO_TOKEN
+  access_token: process.env.MERCADO_PAGO_TOKEN,
 });
 
 export default async function handler(req, res) {
-
-  if (req.method !== "POST") {
-    return res.status(200).json({
-      mensagem: "Backend PIX funcionando"
-    });
-  }
-
   try {
-
-    const { nome, jogo, palpite } = req.body;
-
-    const pagamento = await mercadopago.payment.create({
+    const payment_data = {
       transaction_amount: 5,
-      description: `Palpite ${jogo}`,
+      description: "Bolão Copa",
       payment_method_id: "pix",
-
       payer: {
-        email: `user${Date.now()}@gmail.com`,
-        first_name: nome
-      }
-    });
+        email: "teste@teste.com",
+      },
+    };
+
+    const response = await mercadopago.payment.create(payment_data);
 
     res.status(200).json({
-      qr_code:
-        pagamento.body.point_of_interaction.transaction_data.qr_code,
-
+      qr_code: response.body.point_of_interaction.transaction_data.qr_code,
       qr_code_base64:
-        pagamento.body.point_of_interaction.transaction_data.qr_code_base64,
-
-      status: pagamento.body.status
+        response.body.point_of_interaction.transaction_data.qr_code_base64,
     });
-
-  } catch (erro) {
-
-    console.log(erro);
-
+  } catch (error) {
+    console.log(error);
     res.status(500).json({
-      erro: "Erro ao gerar PIX"
+      error: error.message,
     });
-
   }
 }
