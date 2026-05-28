6e8473b98d6ba2346c94e57abe99280f396fb899
carlosmarleynx7 <carlosmarleycastro852@gmail.com>
2026-05-28 12:41:58 -0300

    Update pix.js

--- api/pix.js
+++ api/pix.js
@@ -6,29 +6,27 @@ mercadopago.configure({
 
 export default async function handler(req, res) {
   try {
-    const payment_data = {
+    const payment = await mercadopago.payment.create({
       transaction_amount: 5,
       description: "Bolão Copa",
       payment_method_id: "pix",
       payer: {
-        email: "testuser4422631723884201271@testuser.com",
+        email: "teste@test.com",
       },
-    };
-
-    const response = await mercadopago.payment.create(payment_data);
+    });
 
     res.status(200).json({
-      qr_code:
-        response.body.point_of_interaction.transaction_data.qr_code,
+      id: payment.body.id,
+      qr_code: payment.body.point_of_interaction.transaction_data.qr_code,
       qr_code_base64:
-        response.body.point_of_interaction.transaction_data.qr_code_base64,
+        payment.body.point_of_interaction.transaction_data.qr_code_base64,
     });
   } catch (error) {
     console.log(error);
 
     res.status(500).json({
       error: error.message,
-      details: error.response?.body,
+      detalhes: error,
     });
   }
 }
